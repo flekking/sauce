70a21a6f629288a6e3df13c05eab4efb16d033c1
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-11-18 15:35:40 +0700

    Make EloquentServiceProvider

---
+++ src/Providers/EloquentServiceProvider.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Fk\Sauce\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Fk\Sauce\Database\Eloquent\Kernel as EloquentKernel;
+use Fk\Sauce\Database\Eloquent\EloquentException;
+use Fk\Sauce\Foundation\Models\Kernel;
+
+class EloquentServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerMorphAliasses();
+    }
+
+    /**
+     * @return void
+     */
+    private function registerMorphAliasses()
+    {
+        $kernels = $this->getEloquentKernels();
+        foreach ($kernels as $kernel) {
+            $morphAliasses = $kernel::relationMorphAliasses();
+            Relation::morphMap($morphAliasses);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    private function getEloquentKernels()
+    {
+        $kernels = config('core.eloquent.kernels');
+        if (is_string($kernels)) $kernels = [$kernels];
+        $kernels[] = Kernel::class;
+
+        foreach ($kernels as $kernel) {
+            if ( ! (new $kernel) instanceof EloquentKernel) {
+                throw EloquentException::new()
+                    ->problem('eloquent_kernel_does_not_extend_abstraction', [
+                        'kernel' => $kernel,
+                    ])
+                    ->toss();
+            }
+        }
+
+        return $kernels;
+    }
+}
