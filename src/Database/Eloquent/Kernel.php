874d6263d481a4d6432da1148eb4e8ea8cd106ca
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-11-18 15:35:25 +0700

    Make base Eloquent Kernel

---
+++ src/Database/Eloquent/Kernel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Fk\Sauce\Database\Eloquent;
+
+abstract class Kernel
+{
+    /**
+     * @static
+     * @return array
+     */
+    public static function relationMorphAliasses() : array
+    {
+
+    }
+}
