b1196341d9808de79bf36dfc8e9a9bf07aad7e44
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-11-24 15:02:03 +0700

    Make sample Macro Kernel on Foundation

---
+++ src/Foundation/Macro/Kernel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Fk\Sauce\Foundation\Macro;
+
+use Fk\Sauce\Macro\Kernel as MacroKernel;
+
+class Kernel extends MacroKernel
+{
+    /**
+     * @return array
+     */
+    public static function blueprints() : array
+    {
+        return [];
+    }
+
+    /**
+     * @return array
+     */
+    public static function rules() : array
+    {
+        return [];
+    }
+}
