3e3b96e236b0381458039b4e74eeea3f63960d26
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-11-24 15:01:08 +0700

    Make Rule Macro Contract

---
+++ src/Macro/Contracts/Rule.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Fk\Sauce\Macro;
+
+interface Rule
+{
+    /**
+     * @var string
+     */
+    const VALIDATE_METHOD = 'validate';
+
+    /**
+     * @var string
+     */
+    const REPLACE_METHOD = 'replace';
+
+    /**
+     * @param  string  $attribute
+     * @param  mixed   $value
+     * @param  array   $parameters
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return boolean
+     */
+    public function validate($attribute, $value, $parameters, $validator) : bool;
+
+    /**
+     * @param  string  $message
+     * @param  string  $attribute
+     * @param  string  $rule
+     * @param  array   $parameters
+     * @return string
+     */
+    public function replace($message, $attribute, $rule, $parameters) : string;
+}
