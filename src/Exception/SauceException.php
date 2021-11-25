6059eadfdb2eac8a7e8cb8cc6416328b35f261f2
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-11-25 10:57:38 +0700

    Relocate the Exception traits

--- src/Exception/SauceException.php
+++ src/Exception/SauceException.php
@@ -11,10 +11,48 @@ use Fk\Sauce\Exception\Traits\{
 
 class SauceException extends Exception
 {
-    use Generate, Problem, Toss;
-
     /**
      * @var array
      */
     protected $messages = [];
+
+    /**
+     * @static
+     * @return  \Exception
+     */
+    public static function new() : Exception
+    {
+        return new self;
+    }
+
+    /**
+     * @param  mixed  $problem
+     * @param  array  $replacers
+     * @return void
+     */
+    public function problem($problem, $replacers = [])
+    {
+        // Get the message from stored problem.
+        $message = $this->messages[$problem];
+
+        // Replace the string with specified arguments.
+        foreach ($replacers as $index => $replace) {
+            $index = ':'.$index;
+            $replace = is_array($replace) ? json_encode($replace) : $replace;
+
+            $message = Str::replaceFirst($index, $replace, $message);
+        }
+
+        $this->message = $message;
+    }
+
+    /**
+     * Help the chaining method to return \Exception instance by default.
+     * 
+     * @return \Exception
+     */
+    public function toss() : Exception
+    {
+        return $this;
+    }
 }
