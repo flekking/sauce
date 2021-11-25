f7a3a5f8b96f7b77f0cc488c735e34315aa6c8a1
agusmikevar <90199402+agusmikevar@users.noreply.github.com>
2021-11-25 17:08:33 +0700

    Make own GeneratorCommand

---
+++ src/Console/GeneratorCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Fk\Sauce\Console;
+
+use Illuminate\Console\GeneratorCommand as Command;
+use Illuminate\Support\Str;
+
+abstract class GeneratorCommand extends Command
+{
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     * @return string
+     *
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    protected function buildClass($name)
+    {
+        $stub = $this->files->get($this->getStub());
+
+        return $this
+            ->replaceContent($stub)
+            ->replaceNamespace($stub, $name)
+            ->replaceClass($stub, $name);
+    }
+
+    /**
+     * Replace the specified contents.
+     * 
+     * @param  string  $stub
+     * @param  string  $name
+     * @return \Fk\Sauce\Console\GeneratorCommand
+     */
+    protected function replaceContent(&$stub)
+    {
+        $data = $this->getContentToBeReplaced($name);
+
+        foreach ($data as $index => $value) {
+            $index = '{{ '.$index.' }}';
+            $stub = Str::replace($index, $value, $stub);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Generate content to be replaced on stub.
+     * 
+     * @param  string  $name
+     * @return array
+     */
+    protected function getContentToBeReplaced($name)
+    {
+        return [];
+    }
+}
